22eb7a0d4ceb0e8348c2e84ef1af6c423b738478
Fathi <ahmedmhmd0237@gmail.com>
2025-07-06 00:47:56 +0200

    initial commit

--- SurvayBasket/Contracts/Common.cs/RequestFilters.cs
+++ SurvayBasket/Contracts/Common.cs/RequestFilters.cs
@@ -3,8 +3,8 @@
 namespace SurvayBasket.Contracts.Common.cs
 {
     public record RequestFilters
-    {
-        public int PageNum { get; set; } = 1;
+    {  public int PageNum { get; set; } = 1;
+      
 
         [Range(1, 10)]
         public int PageSize { get; set; } = 10;
@@ -12,6 +12,7 @@ namespace SurvayBasket.Contracts.Common.cs
         public string? SearchValue { get; set; }
 
         public string? SortColumn { get; init; }
+        
         public string? SortDirection { get; init; } = "ASC";
     }
 }
